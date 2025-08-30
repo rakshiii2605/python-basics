488dca6918527ae756d9644924a1c9327dda550d
A. Logarakshika <rakshikaa2006@gmail.com>
2025-08-30 12:05:39 +0530

    Create write_file.py

---
+++ Week3_Python_Basics/write_file.py
@@ -0,0 +1,6 @@
+# Open the file in write mode ('w')
+with open("example.txt", "w") as file:
+    file.write("Hello, this is the first line of the file!\n")
+    file.write("File handling in Python is simple and powerful.\n")
+
+print("Data written successfully into example.txt")
