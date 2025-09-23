395ec40924c81c0e0f7f33f1c831ef757c50f3e9
A. Logarakshika <rakshikaa2006@gmail.com>
2025-09-23 05:54:17 +0530

    Create random_demo.py

---
+++ Week5_Modules_Libraries/random_demo.py
@@ -0,0 +1,10 @@
+import random
+
+print("Random number between 1 and 100:", random.randint(1, 100))
+
+# Random choice from a list
+fruits = ["apple", "banana", "cherry", "mango"]
+print("Random fruit:", random.choice(fruits))
+
+# Simulate a dice roll
+print("Dice roll:", random.randint(1, 6))
