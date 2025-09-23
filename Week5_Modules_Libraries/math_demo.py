b1f9858abc8af5654cdd00a7a8e003f0fa6d132a
A. Logarakshika <rakshikaa2006@gmail.com>
2025-09-23 05:52:06 +0530

    Create math_demo.py

---
+++ Week5_Modules_Libraries/math_demo.py
@@ -0,0 +1,7 @@
+import math
+
+print("Square root of 25:", math.sqrt(25))
+print("Factorial of 5:", math.factorial(5))
+print("2 raised to the power 3:", math.pow(2, 3))
+print("Pi value:", math.pi)
+print("Sine of 90 degrees:", math.sin(math.radians(90)))
